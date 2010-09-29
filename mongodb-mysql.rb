b191b894006fba5858f9c2aa691287ea68a209b2
Руслан Корнев <oganer@gmail.com>
2010-09-29 10:59:30 +0400

    Блочная вставка в mysql, используя partition's

--- mongodb-mysql.rb
+++ mongodb-mysql.rb
@@ -1,61 +1,74 @@
 require 'rubygems'
-require 'benchmark'
 require 'mysql'
 
-# benchmark cycle
-def cycle x
-  x.times{ |i|
-    doc = {
-      "text" => "i am any text",
-      "count" => i,
-      "coords" => {"x" => 100, "y" => 200, "z" => i}
-    }
-    yield doc
-  }
+def flush_mysql
+  i = 1
+  mysql = Mysql.new('localhost', 'root')
+  db_name = "parts_#{i}"
+  mysql.query("DROP DATABASE IF EXISTS `#{db_name}`")
+  mysql.query("CREATE DATABASE `#{db_name}`")
+  mysql.query("USE #{db_name}")
+
+  1.times do |i|
+    table_name = "prices_#{i}"
+    mysql.query("DROP TABLE IF EXISTS `#{table_name}`")
+    mysql.query("CREATE TABLE `#{table_name}` (
+       `id` int(11) NOT NULL AUTO_INCREMENT,
+       `job_id` int(11) DEFAULT NULL,
+       `job_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `goods_id` int(11) DEFAULT NULL,
+       `supplier` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `count` decimal(10,0) DEFAULT NULL,
+       `initial_cost` decimal(10,3) DEFAULT NULL,
+       `result_cost` decimal(10,3) DEFAULT NULL,
+       `margin` decimal(10,3) DEFAULT NULL,
+       `manufacturer` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `catalog_number` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `inn` varchar(12) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `kpp` varchar(9) COLLATE utf8_unicode_ci DEFAULT NULL,
+       `estimate_days` int(11) DEFAULT NULL,
+       `created_at` datetime DEFAULT NULL,
+       `updated_at` datetime DEFAULT NULL,
+       PRIMARY KEY (`id`, `catalog_number`, `manufacturer`),
+       #UNIQUE KEY `ggg` (`catalog_number`(100),`manufacturer`(100), `id`),
+       KEY `rrr` (`catalog_number`(100))
+    ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+	PARTITION BY key(`catalog_number`, `manufacturer`)         PARTITIONS 1000
+	")
+
+    100_000.times do |i|
+      query = "INSERT INTO  `#{table_name}` (
+          `id` ,
+          `job_id` ,
+          `job_title` ,
+          `goods_id` ,
+          `supplier` ,
+          `title` ,
+          `count` ,
+          `initial_cost` ,
+          `result_cost` ,
+          `margin` ,
+          `manufacturer` ,
+          `catalog_number` ,
+          `inn` ,
+          `kpp` ,
+          `estimate_days` ,
+          `created_at` ,
+          `updated_at`
+          )
+          VALUES "
+
+      1_000.times do |i|
+        query = query + "( NULL ,  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(1_000_000).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '#{rand(100).to_s}',  '2010-08-07 11:46:15',  '2010-08-07 11:46:15' ),"
+      end
+
+      query.chop!
+
+      mysql.query(query)
+
+    end
+  end
 end
 
-def flush_mysql mysql
-  mysql.query("DROP TABLE IF EXISTS `insert_bm`")
-  mysql.query("DROP TABLE IF EXISTS `insert_bm_coords`")
-  mysql.query("CREATE TABLE `insert_bm_coords`(
-    `id` int unsigned not null auto_increment,
-    `x` int not null,
-    `y` int not null,
-    PRIMARY KEY (`id`)
-  ) ENGINE=INNODB")
-  mysql.query("CREATE TABLE `insert_bm`(
-    `id` int unsigned not null auto_increment,
-    `text` varchar(255) not null,
-    `count` int not null,
-    `coords_id` int unsigned not null,
-    PRIMARY KEY (`id`)
-  ) ENGINE=INNODB")
-  # empty init entries
-  mysql.query("INSERT insert_bm_coords(x,y) VALUES(0, 0)")
-  mysql.query("INSERT insert_bm(text, count, coords_id) VALUES('', 0, #{mysql.insert_id()})")
-end
-
-def flush_mongo mongo_cnn
-  mongo_cnn.collection("bmdb").drop
-  mongo = mongo_cnn.collection("bmdb")
-  mongo.insert({"stub" => "i am empty init doc"})
-  mongo
-end
-
-mysql = Mysql.new('localhost', 'root', '', 'test')
-
-# do benchmarking
-Benchmark.bm {|x|
-  points = [1_000, 10_000, 100_000, 500_000, 1_000_000]
-
-  points.each{|c|
-    flush_mysql mysql
-    
-    x.report("mysql #{c.to_s} :") {
-      cycle(c){|doc|
-        mysql.query("INSERT insert_bm_coords(x,y) VALUES(#{doc["coords"]["x"]}, #{doc["coords"]["y"]})")
-        mysql.query("INSERT insert_bm(text, count, coords_id) VALUES('#{doc["text"]}', #{doc["count"]}, #{mysql.insert_id()})")
-      }
-    }
-  }
-}
+flush_mysql
